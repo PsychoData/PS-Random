24a446d7ef5df04af48820bba8ef97ea810d2ce6
Kevin Crouch <PsychoData91@gmail.com>
2020-11-16 09:43:22 -0500

    Initial Commit IMCEAEX-ToX500
    convert cached/legacyExchangeDN addresses to x500's

---
+++ IMCEAEX-ToX500.ps1
@@ -0,0 +1,10 @@
+﻿function IMCEAEX-ToX500 ($IMCEAEX)
+{
+    $X500 = $IMCEAEX -replace "_",'/' -replace "\+20"," " -replace "\+28","(" -replace "\+29",")" -replace "^IMCEAEX-",'' -replace "@(\w+.)+\w$",""-replace "^(.*)","X500:`$1" 
+    return $X500 
+}
+
+
+IMCEAEX-ToX500 -IMCEAEX "IMCEAEX-_O=FIRST+20ORGANIZATION_OU=EXCHANGE+20ADMINISTRATIVE+20GROUP+20+28FYDIBOHF23SPDLT+29_CN=RECIPIENTS_CN=Comp+20Procurement9ca@domain.com"
+
+
